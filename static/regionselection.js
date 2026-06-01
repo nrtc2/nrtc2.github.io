a9368cdbc3ae7709a39b83ed1c7f0ec136d386eb
n-try <142988995+nrtc2@users.noreply.github.com>
2026-06-01 10:02:17 +0700

    Create regionselection.js

---
+++ static/regionselection.js
@@ -0,0 +1,20 @@
+const e = new RegionSelection
+e.onSelection(function(sx, sy, ex, ey) {
+    const d = Date.now()
+    let s = 0
+    let c = 0
+    for (let y = sy; y <= ey; y++) {
+        for (let x = sx; x <= ex; x++) {
+            if (getCharInfoXY(x, y).char !== " ") {
+                s = writeCharAt(" ", 0, x, y)
+                if (s !== 0) {
+                    c += 1
+                }
+            }
+        }
+    }
+    printMsg("script", 0, `Region voided: ${sx}, ${-sy} to ${ex}, ${-ey} |
+Time taken: ${(Date.now()-d)/1000} seconds |
+Chars successfully cleared: ${c}/${(ex-sx+1)*(ey-sy+1)}`, !1)
+})
+e.startSelection()
