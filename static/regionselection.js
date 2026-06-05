b57d9efe119fd86a8554e6b35911e964c64b634c
n-try <142988995+nrtc2@users.noreply.github.com>
2026-06-05 19:32:17 +0700

    Create regionselection.js

---
+++ static/regionselection.js
@@ -0,0 +1,69 @@
+function t(n, t) {
+	n -= 500;
+	return t()[n]
+}
+const n = t;
+
+function r() {
+	const n = ["850gVBGlj", "1764788OwBzon", "char", "7Ifomtx", "1548kuZqlQ", " seconds\nC", "BhMOZ", "ssfully cl", "now", "tion", "showToast", "Region voi", "tlYht", "352191GDGMJv", "yRSES", "nVOyP", "startSelec", "deAcm", "npMKz", "onSelectio", "n: ", "eared: ", "XpMkz", "13245510tlYcah", "54acNexs", "hfYWg", "DknLR", "ded: ", "564568aADlnd", "LJuoA", " to ", "TATpw", "7251813SuTpWU", "WRbts", "\nTime take", "33918QcIAlM", "hars succe"];
+	return (r = function() {
+		return n
+	})()
+}! function(n) {
+	const e = t,
+		r = n();
+	for (;;) try {
+		if (575490 === parseInt(e(519)) / 1 + -parseInt(e(507)) / 2 + parseInt(e(530)) / 3 * (parseInt(e(510)) / 4) + -parseInt(e(506)) / 5 * (parseInt(e(504)) / 6) + parseInt(e(509)) / 7 * (-parseInt(e(534)) / 8) + parseInt(e(501)) / 9 + parseInt(e(529)) / 10) break;
+		r.push(r.shift())
+	} catch (n) {
+		r.push(r.shift())
+	}
+}(r);
+const sel = new RegionSelection;
+sel[n(525) + "n"](function(n, t, e, r) {
+	const c = n,
+		o = {
+			npMKz: function(n, t) {
+				return n <= t
+			},
+			WRbts: function(n, t) {
+				return n <= t
+			},
+			TATpw: function(n, t) {
+				return n !== t
+			},
+			LJuoA: function(n, t, e) {
+				return n(t, e)
+			},
+			hfYWg: function(n, t, e, r, c) {
+				return n(t, e, r, c)
+			},
+			BhMOZ: function(n, t) {
+				return n !== t
+			},
+			DknLR: function(n, t) {
+				return n / t
+			},
+			yRSES: function(n, t) {
+				return n - t
+			},
+			deAcm: function(n, t) {
+				return n * t
+			},
+			nVOyP: function(n, t) {
+				return n + t
+			},
+			XpMkz: function(n, t) {
+				return n + t
+			},
+			tlYht: function(n, t) {
+				return n - t
+			}
+		},
+		u = Date[c(514)]();
+	let s = 0,
+		a = 0;
+	for (let u = t; o[c(524)](u, r); u++)
+		for (let t = n; o[c(502)](t, e); t++) o[c(500)](o[c(535)](getCharInfoXY, t, u)[c(508)], " ") && (s = o[c(531)](writeCharAt, " ", 0, t, u), o[c(512)](s, 0) && (a += 1));
+	w[c(516)](c(517) + c(533) + n + ", " + -t + c(536) + e + ", " + -r + (c(503) + c(526)) + o[c(532)](o[c(520)](Date[c(514)](), u), 1e3) + (c(511) + c(505) + c(513) + c(527)) + a + "/" + o[c(523)](o[c(521)](o[c(520)](e, n), 1), o[c(528)](o[c(518)](r, t), 1)), 1500)
+}), sel[n(522) + n(515)]();
