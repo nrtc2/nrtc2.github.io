5486e002e15be68e2fa99e54d7eded21c0939e3c
n-try <142988995+nrtc2@users.noreply.github.com>
2026-06-22 12:12:21 +0700

    Update misgigndn.js

--- static/misgigndn.js
+++ static/misgigndn.js
@@ -1,6 +1,34 @@
 const data = {},
     d = document;
 
+const misogLevels = [
+    [0, "no misogin", "#00CC78"],
+    [5, "low misogin", "#FFD635"],
+    [20, "mid misogin", "#FF4500"],
+    [80, "high misogin", "#BE0039"],
+    [250, "TOO MISOGIN", "#515252"],
+    [500, "WTFQY8 syndrome", "#FFFFFF"]
+];
+
+function getThresoldValue(inputNumber, thresholdArray) {
+    if (!Array.isArray(thresholdArray)) throw new TypeError("The second argument must be an array");
+    if (!thresholdArray.every(function (a) {
+        return a.length >= 2
+    })) throw TypeError("All entries must have 2 or more values")
+
+    const sorted = thresholdArray.toSorted(function (a, b) {
+        return a[0] - b[0]
+}).toReversed()
+
+    for (const row of sorted) {
+        if (row[0] <= inputNumber) {
+            return row.toSpliced(0, 1);
+        }
+    }
+    
+    return null;
+}
+
 async function fetchNotReiny(path) {
     let response = await fetch(`https://api.github.com/repos/nrtc2/nrtc2.github.io/contents/${path}`);
     let data = await response.json();
@@ -20,7 +48,11 @@ async function init() {
       data[k] = parsed[k];
     }
 
-    appendOptions(data, d.getElementById("sel"))
+    appendOptions(data, d.getElementById("sel"));
+    d.getElementById("sel").addEventListener("input", function (e) {
+        const va = e.target.value;
+        d.getElementsByTagName("p")[0].innerText = getThresoldValue(data[va], misogLevels)
+    })
 }
 
 init();
