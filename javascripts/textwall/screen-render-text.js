f168399687ee0df93d5f64ff9a9fadfa969d608c
n-try <142988995+nrtc2@users.noreply.github.com>
2026-06-22 18:38:58 +0700

    Create screen-render-text.js

---
+++ javascripts/textwall/screen-render-text.js
@@ -0,0 +1,38 @@
+async function scr(u) {
+    function paginateText(text, max = 78, ln = 18) {
+        let lines = [];
+        let line = "";
+        let len = 0;
+
+        for (const ch of text) {
+            if (ch === "\n") {
+                lines.push(line);
+                line = "";
+                len = 0;
+                continue;
+            }
+
+            if (len >= max) {
+                lines.push(line);
+                line = "";
+                len = 0;
+            }
+
+            line += ch;
+            len++;
+        }
+
+        lines.push(line.padEnd(max, " "));
+        for (let e = 0; e < ln-lines.length; e++) lines.push(" ".repeat(max))
+        return lines.slice(0, ln).join("\n");
+    }
+
+    await writeTextAt(
+        paginateText(`${u}`).split("\n")
+        .slice(0, 18)
+        .map(e => e.slice(0, 78))
+        .join("\n"),
+        0, 21, -9, 0
+    )
+}
+0
