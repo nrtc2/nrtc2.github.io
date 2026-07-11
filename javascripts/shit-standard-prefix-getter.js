85e678db004f38ccbfc312da248444ec8da839e6
The179UCETile <khang240582@gmail.com>
2026-07-11 14:18:15 +0700

    Update shit-standard-prefix-getter.js

--- javascripts/shit-standard-prefix-getter.js
+++ javascripts/shit-standard-prefix-getter.js
@@ -4,7 +4,7 @@ function ss(illion, c = false) {
   const r = [
     "k m b t qa q sa s o n", " u d t qa q sa s o n", " de ve te qe qf se sf oe ne", " cn dn tn qn qo sn so on nn",
     " mi mm ni pi fi ai zi yi ri qi mc dc tc td pc hc hd oc ec is mis dis tis trs pis his hps ois eis", " mc dc tc td pc hc hd oc ec", " qi is tt tr pt ht hr ot et", " hl dh th tl ph hh he ol el",
-    " kq mq gq tq pq eq zq yq rq qq hk tk td pk ek zk yk nk ik ike ikd ikt itr ikp iex ikz iky ikr", " e d t tr p ex z y r", " dk ik to td po eo zo yo no", " hu bu tu tv pu eu zu yu nu",
+    " kq mq gq tq pq eq zq yq rq qq hk dk tk td pk ek zk yk nk ik ike ikd ikt itr ikp iex ikz iky ikr", " e d t tr p ex z y r", " dk ik to td po eo zo yo no", " hu bu tu tv pu eu zu yu nu",
     " kj mj gj aj lj fj jj sj bj gp gm sp vj mp pj gg kp oj pp hj", " gp gm bp gb ab lb fb jb sb bb", " hb mb gub aub lub fub jub sub bub",
     " hz oz nz dz uz ez fz sz bz", " gz ay hy ky py sy px ny zy", " aw bw gw dw tw iw kw lw sw",
     " hÞ dÞ tÞ tß aÞ sÞ sß cÞ nÞ eÞ að hð lð oð pð cð wð að gð oð tø sø lø jø gø iø xø wø mø hø", " eÞ oð hø uæ væ sæ tæ hæ næ aæ tœ iœ uœ qœ tſ bœ tƹ nœ zœ", " aæ nſ bſ kſ gſ pſ vſ uſ lſ",
